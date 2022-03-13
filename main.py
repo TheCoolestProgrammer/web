a33ba0eef6908e96ffbc4432568e170c18754d85
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-03-13 17:31:08 +0500

    задача "цвет каюты"

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route("/distribution")
-def list_proof():
+@app.route("/table/<sex>/<int:age>")
+def table(sex,age):
 
-    main_list=[1,2,3,4,1,2,5555]
-    return render_template('index.html', main_list=main_list)
+    return render_template('index.html',sex=sex,age=age)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
