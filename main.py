54bede49f9f60efd647c3eb4940789030ec174dd
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-03-13 16:49:35 +0500

    задача "по каютам!"

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route("/list_proof/<listt>")
-def list_proof(listt):
+@app.route("/distribution")
+def list_proof():
 
     main_list=[1,2,3,4,1,2,5555]
-    return render_template('index.html', list=listt, main_list=main_list)
+    return render_template('index.html', main_list=main_list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
