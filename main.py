f49c8083911e0e00e1df9380e04c6629a67574b5
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-03-13 19:37:45 +0500

    задача "личная карточка"

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route("/table/<sex>/<int:age>")
-def table(sex,age):
-
-    return render_template('index.html',sex=sex,age=age)
+@app.route("/member")
+def table():
+    with open("templates/members.json", "rt", encoding="utf8") as f:
+        names_list = json.loads(f.read())
+    return render_template('index.html',members=names_list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
