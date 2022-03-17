82abef5889af29393fb0102f15093c3cf71ff61f
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-03-17 18:17:24 +0500

    заготовка к sql

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 import json
+from data import db_session
+
+db_session.global_init("db/blogs.db")
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+def main():
+    app.run()
 
 
-@app.route('/')
-@app.route("/member")
-def table():
-    with open("templates/members.json", "rt", encoding="utf8") as f:
-        names_list = json.loads(f.read())
-    return render_template('index.html',members=names_list)
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
