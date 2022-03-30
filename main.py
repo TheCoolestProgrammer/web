a4ffe590ee923df3321b961b4ed5cb0f8c0e2068
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-03-30 15:08:42 +0500

    something went wrong

--- main.py
+++ main.py
@@ -1,11 +1,87 @@
-from flask import render_template
+
+from flask import render_template, redirect
 from flask import Flask
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
+from flask_login import LoginManager, login_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, EmailField, BooleanField, StringField, TextAreaField
+from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db_session.global_init("db/blogs.db")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+def set_password(self, password):
+    self.hashed_password = generate_password_hash(password)
+
+def check_password(self, password):
+    return check_password_hash(self.hashed_password, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 @app.route("/")
 def index():
@@ -14,36 +90,6 @@ def index():
     jobs = db_sess.query(Jobs).all()
 
     return render_template("index.html", jobs=jobs)
-def main():
-    job = Jobs()
-    job.team_leader = "captain Jack Sparrow"
-    job.job = "pirating"
-    job.work_size = 24
-    job.collaborators = "Tobie Mckguire, Batman, Ryan Gosling"
-    job.is_finished =False
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
-
-    job = Jobs()
-    job.team_leader = "Leon from brawl stars"
-    job.job = "bank robbery"
-    job.work_size = 2
-    job.collaborators = "Frank, deadpool, terminator"
-    job.is_finished = False
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
-
-    job = Jobs()
-    job.team_leader = "russian"
-    job.job = "aneckdot(need american)"
-    job.work_size = 128
-    job.collaborators = "german"
-    job.is_finished = False
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
 
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
