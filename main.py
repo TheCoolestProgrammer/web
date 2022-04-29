d7f800adccddf32407d6aabe66f60a6949aacf9a
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-04-29 19:52:58 +0500

    пейзажи марса 2

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_wtf import file
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route("/galery")
+@app.route("/galery", methods=['POST', 'GET'])
 def table():
     folder = Path("static/image/mars_images")
-    max_folder_id = len(list(folder.iterdir())) - 1
+    max_folder_id = len(list(folder.iterdir()))
+    if request.method == 'POST':
+        file = request.files["file"]
+        file.save(f"static/image/mars_images/mars{max_folder_id+1}.png")
+        max_folder_id +=1
     return render_template('index.html', max_folder_id=max_folder_id)
 
 if __name__ == '__main__':
